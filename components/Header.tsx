f09e536431921ce38779cc9d0bbc016be6d896f0
Arbaz Yousuf <contactme797@gmail.com>
2019-11-08 00:44:47 +0500

    header finalize

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Icon } from "native-base";
 
-export function Header() {
+export function Header(props) {
     return (
-            <View style={{ flexDirection: 'row', height: 50, marginTop: 20 }}>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Icon
-                        type="FontAwesome5"
-                        name="bars"
-                        style={{ color: "white", fontSize: 16 }}
-                    />
-                </View>
-                <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>
-                        To do list
-                     </Text>
-                </View>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-                </View>
+        <View style={{ flexDirection: 'row', height: 50, marginTop: 20 }}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                {props.isMenu ? <Icon
+                    type="FontAwesome5"
+                    name="bars"
+                    style={{ color: "white", fontSize: 16 }}
+                /> : null}
             </View>
+            <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>
+                    {props.title}
+                </Text>
+            </View>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+            </View>
+        </View>
     );
 }
 
